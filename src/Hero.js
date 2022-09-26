e90c295224f3b2d8f570c646f74a6cd78161b487
binarypsilocybin <53083287+binarypsilocybin@users.noreply.github.com>
2022-09-26 17:22:12 +0100

    1 file modified

--- src/Hero.js
+++ src/Hero.js
@@ -8,9 +8,17 @@ const Hero = () => {
   return (
     <section className="hero">
       <div className="hero-center">
-        <article></article>
+        <article className="hero-info">
+          <h1>Payments infraestructure for the internet</h1>
+          <p>
+            Millions of companies of all sizes—from startups to Fortune 500s—use
+            Stripe’s software and APIs to accept payments, send payouts, and
+            manage their businesses online.
+          </p>
+          <button className="btn">Start now</button>
+        </article>
         <article className="hero-images">
-          <img src={phoneImg} className="phone-img" alt="" />
+          <img src={phoneImg} className="phone-img" alt="phone" />
         </article>
       </div>
     </section>
