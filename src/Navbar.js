b9cf7efd9d49d0d54cc4903a7e476ccc1e086234
binarypsilocybin <53083287+binarypsilocybin@users.noreply.github.com>
2022-09-26 20:11:49 +0100

    1 file modified

--- src/Navbar.js
+++ src/Navbar.js
@@ -5,6 +5,10 @@ import { useGlobalContext } from './context';
 
 const Navbar = () => {
   const { openSidebar, openSubmenu, closeSubmenu } = useGlobalContext();
+  const displaySubmenu = (e) => {
+    console.log('hello world');
+    openSubmenu();
+  };
   return (
     <nav className="nav">
       <div className="nav-center">
@@ -16,13 +20,19 @@ const Navbar = () => {
         </div>
         <ul className="nav-links">
           <li>
-            <button classNam="link-btn">products</button>
+            <button classNam="link-btn" onMouseOver={displaySubmenu}>
+              products
+            </button>
           </li>
           <li>
-            <button classNam="link-btn">developers</button>
+            <button classNam="link-btn" onMouseOver={displaySubmenu}>
+              developers
+            </button>
           </li>
           <li>
-            <button classNam="link-btn">company</button>
+            <button classNam="link-btn" onMouseOver={displaySubmenu}>
+              company
+            </button>
           </li>
         </ul>
         <button className="btn signin-btn "></button>
